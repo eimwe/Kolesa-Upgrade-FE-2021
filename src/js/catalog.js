47f34354e16860f92e3b12e0962320690de54771
eimwe <prokopenkoserge@inbox.ru>
2021-10-10 22:13:00 +0600

    Добавил функцию для отрисовки карточек товаров
    1. customizeCardTemplate() - создает карточки на основе шаблона, подставляя значения, переданные в итераторы
    2. forEach() итерируется по массивам объектов apparel и accessories и наполняет карточки значениями из массива

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -1,5 +1,52 @@
 import '../css/catalog.scss';
-// import { CLOTHES, MISC } from './shopcards';
+import { CLOTHES, MISC } from './shopcards';
 
-// const apparel = CLOTHES;
-// const accessories = MISC;
+const apparel = CLOTHES;
+const accessories = MISC;
+
+const customizeCardTemplate = (id, img, title, price, isTagged) => {
+    const cardTemplate = document.getElementById('shopcard-template');
+    const shopCard = cardTemplate.content.firstElementChild.cloneNode(true);
+    const cardImgWrapper = shopCard.querySelector('.shop-card__image');
+    const imagePath = 'src/assets/images/shop/';
+    const cardImage = new Image();
+
+    shopCard.dataset.id = id;
+
+    cardImage.src = imagePath + img;
+    cardImage.setAttribute('alt', title);
+    cardImgWrapper.appendChild(cardImage);
+
+    shopCard.querySelector('.base-card__title').textContent = title;
+    shopCard.querySelector('.shop-card__price').textContent = `${price} баллов`;
+
+    if (isTagged) {
+        const tagNode = document.createElement('SPAN');
+
+        tagNode.classList.add('shop-card__tag', 'shop-card__tag--new');
+        tagNode.appendChild(document.createTextNode('New'));
+        cardImgWrapper.appendChild(tagNode);
+    }
+
+    return shopCard;
+};
+
+apparel.forEach((card) => {
+    const {
+        id, img, title, price, isTagged,
+    } = card;
+    const customizedCard = customizeCardTemplate(id, img, title, price, isTagged);
+    const cardContainer = document.getElementById('shop-front');
+
+    cardContainer.appendChild(customizedCard);
+});
+
+accessories.forEach((card) => {
+    const {
+        id, img, title, price, isTagged,
+    } = card;
+    const customizedCard = customizeCardTemplate(id, img, title, price, isTagged);
+    const cardContainer = document.getElementById('shop-front');
+
+    cardContainer.appendChild(customizedCard);
+});
