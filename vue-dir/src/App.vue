eafea58ccada7f42bb86f92072c99d4ecd8b4843
eimwe <prokopenkoserge@inbox.ru>
2021-10-26 19:38:31 +0600

    Добавил компонент 'Footer'
    1. Создал новый файл для компонента Footer в директории ./components
    2. Вынес разметку футера из файла App.vue и перенес в Footer.vue
    3. Добавил связь с Footer.vue в App.vue

--- vue-dir/src/App.vue
+++ vue-dir/src/App.vue
@@ -304,53 +304,18 @@
             </aside>
         </div>
     </main>
-    <footer class="footer">
-        <div class="wrapper flex-container">
-            <div class="footer__about">
-                <p class="footer__copyright">&#169; Kolesa Group</p>
-                <ul class="socialbar flex-container">
-                    <li>
-                        <a class="socialbar__link socialbar__link--insta"
-                            href="https://www.instagram.com/kolesagroup/"
-                            target="_blank"
-                            title="Мы в Instagram">
-                            Instagram
-                        </a>
-                    </li>
-                    <li>
-                        <a class="socialbar__link socialbar__link--yt"
-                            href="https://www.youtube.com/channel/UC6o6waSdIvmIdvy7qTqxY9A"
-                            target="_blank"
-                            title="Наши видео на YouTube">
-                            YouTube
-                        </a>
-                    </li>
-                    <li>
-                        <a class="socialbar__link socialbar__link--vk"
-                            href="https://vk.com/kolesakrishamarket"
-                            target="_blank"
-                            title="Наша страничка ВКонтакте">
-                            ВКонтакте
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="footer__contacts flex-container">
-                <p class="footer__invitation">
-                    Есть идеи что улучшить?
-                    <span class="line-break">Не знаешь, с кем решить проблему?</span>
-                </p>
-                <button class="btn btn--contact" type="button">Написать</button>
-            </div>
-        </div>
-    </footer>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
+import Footer from './components/Footer.vue';
 
 export default {
-    name: 'App',
+    name:       'App',
+    components: {
+        Footer,
+    },
     data() {
         return {
             clothes: [
