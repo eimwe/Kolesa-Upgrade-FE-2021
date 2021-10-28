3ec7263f4d7083354007ad9409c335c1e7dec00a
eimwe <prokopenkoserge@inbox.ru>
2021-10-28 10:23:10 +0600

    Заменил моки данных в App.vue на JSON ответы с сервера

--- vue-dir/src/App.vue
+++ vue-dir/src/App.vue
@@ -93,9 +93,9 @@
                             :key="item.id"
                             :data-id="item.id">
                             <div class="shop-card__image">
-                                <img :src="require(`./assets/images/shop/` + item.img)">
+                                <img :src="item.mainImage">
                                 <span class="shop-card__tag shop-card__tag--new"
-                                    v-if="item.isTagged">
+                                    v-if="item.isNew">
                                     New
                                 </span>
                             </div>
@@ -164,208 +164,8 @@ export default {
     },
     data() {
         return {
-            clothes: [
-                {
-                    id:        0,
-                    img:       'shirt.webp',
-                    title:     'Футболка "Эволюционируй или сдохни"',
-                    price:     '220',
-                    isTagged:  false,
-                    details:   'Брендированная футболка от Qazaq Republic. Материал: Хлопок 80%, Вискоза 20%',
-                    fullsized: [
-                        'shirt-01.webp',
-                        'shirt-02.webp',
-                        'shirt-03.webp',
-                    ],
-                    thumbs: [
-                        'shirt-thumb-01.webp',
-                        'shirt-thumb-02.webp',
-                        'shirt-thumb-03.webp',
-                    ],
-                },
-                {
-                    id:        1,
-                    img:       'sweatshirt.webp',
-                    title:     'Свитшот',
-                    price:     '320',
-                    isTagged:  false,
-                    details:   'Брендированная толстовка от Qazaq Republic. Материал: Хлопок 80%, Вискоза 20%',
-                    fullsized: [
-                        'sweatshirt-01.webp',
-                        'sweatshirt-02.webp',
-                        'sweatshirt-03.webp',
-                    ],
-                    thumbs: [
-                        'sweatshirt-thumb-01.webp',
-                        'sweatshirt-thumb-02.webp',
-                        'sweatshirt-thumb-03.webp',
-                    ],
-                },
-                {
-                    id:        2,
-                    img:       'dom-shirt.webp',
-                    title:     'Футболка "DOM"',
-                    price:     '250',
-                    isTagged:  true,
-                    details:   'Для прогулок по теневому дому. Материал: Узлы-элементы 80%, Текстовые узлы 20%',
-                    fullsized: [
-                        'dom-shirt-01.webp',
-                        'dom-shirt-02.webp',
-                        'dom-shirt-03.webp',
-                    ],
-                    thumbs: [
-                        'dom-shirt-thumb-01.webp',
-                        'dom-shirt-thumb-02.webp',
-                        'dom-shirt-thumb-03.webp',
-                    ],
-                },
-                {
-                    id:        3,
-                    img:       'survivor-shirt.webp',
-                    title:     'Футболка "Выживший"',
-                    price:     '0',
-                    isTagged:  true,
-                    details:   'Выдается бесплатно тем, кто успешно сдал финальный проект',
-                    fullsized: [
-                        'survivor-shirt-01.webp',
-                        'survivor-shirt-02.webp',
-                        'survivor-shirt-03.webp',
-                    ],
-                    thumbs: [
-                        'survivor-shirt-thumb-01.webp',
-                        'survivor-shirt-thumb-02.webp',
-                        'survivor-shirt-thumb-03.webp',
-                    ],
-                },
-                {
-                    id:        4,
-                    img:       'async-sweatshirt.webp',
-                    title:     'Худи "async/await"',
-                    price:     '320',
-                    isTagged:  false,
-                    details:   'Худи с капюшоном для анонимности. Материал: асинхронность 100%',
-                    fullsized: [
-                        'async-sweatshirt-01.webp',
-                        'async-sweatshirt-02.webp',
-                        'async-sweatshirt-03.webp',
-                    ],
-                    thumbs: [
-                        'async-sweatshirt-thumb-01.webp',
-                        'async-sweatshirt-thumb-02.webp',
-                        'async-sweatshirt-thumb-03.webp',
-                    ],
-                },
-                {
-                    id:        5,
-                    img:       'socks.webp',
-                    title:     'Носочки "Kolesa Group Rocks"',
-                    price:     '75',
-                    isTagged:  false,
-                    details:   'Брендированные носочки. Материал: Хлопок 80%, Милота 20%',
-                    fullsized: [
-                        'socks-01.webp',
-                    ],
-                    thumbs: [
-                        'socks-thumb-01.webp',
-                    ],
-                },
-            ],
-            accessories: [
-                {
-                    id:        6,
-                    img:       'bottle.webp',
-                    title:     'Бутылка для воды',
-                    price:     '100',
-                    isTagged:  false,
-                    details:   'Бутылка для воды с трубочкой',
-                    fullsized: [
-                        'bottle-01.webp',
-                    ],
-                    thumbs: [
-                        'bottle-thumb-01.webp',
-                    ],
-                },
-                {
-                    id:        7,
-                    img:       'snapback.webp',
-                    title:     'Бейсболка Kolesa Group',
-                    price:     '180',
-                    isTagged:  false,
-                    details:   'Брендированный снэпбек',
-                    fullsized: [
-                        'snapback-01.webp',
-                    ],
-                    thumbs: [
-                        'snapback-thumb-01.webp',
-                    ],
-                },
-                {
-                    id:        8,
-                    img:       'coasters.webp',
-                    title:     'Подстаканники Kolesa',
-                    price:     '20',
-                    isTagged:  true,
-                    details:   'Подстаканники в ассортименте',
-                    fullsized: [
-                        'coasters-01.webp',
-                        'coasters-02.webp',
-                        'coasters-03.webp',
-                    ],
-                    thumbs: [
-                        'coasters-thumb-01.webp',
-                        'coasters-thumb-02.webp',
-                        'coasters-thumb-03.webp',
-                    ],
-                },
-                {
-                    id:        9,
-                    img:       'soap.webp',
-                    title:     'Мыло ручной работы "try...catch"',
-                    price:     '40',
-                    isTagged:  true,
-                    details:   'Сварено по особому рецепту. Состав: Глицерин 80%, Слезы верстальщиков 20%',
-                    fullsized: [
-                        'soap-01.webp',
-                        'soap-02.webp',
-                        'soap-03.webp',
-                    ],
-                    thumbs: [
-                        'soap-thumb-01.webp',
-                        'soap-thumb-02.webp',
-                        'soap-thumb-03.webp',
-                    ],
-                },
-                {
-                    id:        10,
-                    img:       'keychain.webp',
-                    title:     'Брелок для AirTag',
-                    price:     '400',
-                    isTagged:  false,
-                    details:   'Брендированный брелок',
-                    fullsized: [
-                        'keychain-01.webp',
-                    ],
-                    thumbs: [
-                        'keychain-thumb-01.webp',
-                    ],
-                },
-                {
-                    id:        11,
-                    img:       'phonecase.webp',
-                    title:     'Защитный чехол для смартфона',
-                    price:     '200',
-                    isTagged:  false,
-                    details:   'Брендированный чехол для твоего смартфона. Материал: Поликарбонат 100%',
-                    fullsized: [
-                        'phonecase-01.webp',
-                        'phonecase-02.webp',
-                    ],
-                    thumbs: [
-                        'phonecase-thumb-01.webp',
-                        'phonecase-thumb-02.webp',
-                    ],
-                },
-            ],
+            clothes:     [],
+            accessories: [],
             everything:  [],
             items:       [],
             isShowModal: false,
@@ -373,12 +173,32 @@ export default {
             user:        '',
         };
     },
+    mounted() {
+
+    },
     created() {
         axios.get('/templates/-_RLsEGjof6i/data')
             .then((response) => {
-                console.log(response);
+                this.clothes = response.data;
+                this.mergeEverything();
+            })
+            .catch((err) => {
+                console.warn(err);
+            })
+            .finally(() => {
+                this.mergeEverything();
             });
-        this.mergeEverything();
+
+        axios.get('/templates/q3OPxRyEcPvP/data')
+            .then((response) => {
+                this.accessories = response.data;
+            })
+            .catch((err) => {
+                console.warn(err);
+            })
+            .finally(() => {
+                this.mergeEverything();
+            });
     },
     methods: {
         passDataToModal(data) {
@@ -397,7 +217,7 @@ export default {
 
         findNewest(array) {
             array.forEach((item, index) => {
-                if (item.isTagged) {
+                if (item.isNew) {
                     array.splice(index, 1);
                     array.unshift(item);
                 }
