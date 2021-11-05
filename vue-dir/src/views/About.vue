7eb65da7424554dc5881d309556da5cd2caeb911
eimwe <prokopenkoserge@inbox.ru>
2021-11-05 17:33:59 +0600

    Переделал компонент About.vue

--- vue-dir/src/views/About.vue
+++ vue-dir/src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <figure class="wallpaper flex-container">
+        <img src="../assets/images/homepage/team.webp" alt="Kolesa Team">
+        <figcaption class="wallpaper__title">Kolesa Team</figcaption>
+    </figure>
 </template>
+
+<style lang="scss">
+@use '../assets/styles/homepage';
+</style>
