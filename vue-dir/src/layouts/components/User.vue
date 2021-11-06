aaee7c24251cae1536e48ed48dfd88ea84a2de54
eimwe <prokopenkoserge@inbox.ru>
2021-11-06 22:09:21 +0600

    Вынес данные из User.vue в Vuex State

--- vue-dir/src/layouts/components/User.vue
+++ vue-dir/src/layouts/components/User.vue
@@ -1,41 +1,30 @@
 <template>
     <a class="user" href="#" title="Перейти в свой профиль">
         <figure class="user__profile flex-container">
-            <img class="user__avatar" :src="avatar" alt="Фото пользователя">
+            <img class="user__avatar" :src="$store.state.userInfo.avatarUrl" alt="Фото пользователя">
             <figcaption class="user__account">
-                <p class="user__name">{{ name }}</p>
-                <span class="user__budget">{{ score }} баллов</span>
+                <p class="user__name">{{ $store.state.userInfo.name }}</p>
+                <span class="user__budget">{{ score }}</span>
             </figcaption>
         </figure>
     </a>
 </template>
 
 <script>
-import axios from '../../axios';
 
 export default {
     name: 'User',
     created() {
         this.getUserData();
     },
-    data() {
-        return {
-            score:  '',
-            name:   '',
-            avatar: '',
-        };
+    computed: {
+        score() {
+            return this.$store.state.userInfo.score ? `${this.$store.state.userInfo.score} баллов` : '';
+        },
     },
     methods: {
         getUserData() {
-            axios.get('/templates/7ZW3y5GAuIge/data')
-                .then((response) => {
-                    this.score = response.data.score;
-                    this.name = response.data.name;
-                    this.avatar = response.data.avatarUrl;
-                })
-                .catch((err) => {
-                    console.warn(err);
-                });
+            this.$store.dispatch('fetchUserInfo');
         },
     },
 };
--- vue-dir/src/store/index.js
+++ vue-dir/src/store/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from '@/axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
+        userInfo: {
+            score:     '',
+            name:      '',
+            avatarUrl: '',
+        },
     },
     mutations: {
+        updateUserInfo(state, response) {
+            state.userInfo = response;
+        },
     },
     actions: {
-    },
-    modules: {
+        fetchUserInfo({ commit }) {
+            axios.get('/templates/7ZW3y5GAuIge/data')
+                .then((response) => {
+                    commit('updateUserInfo', response.data);
+                })
+                .catch((err) => {
+                    console.warn(err);
+                });
+        },
     },
 });
